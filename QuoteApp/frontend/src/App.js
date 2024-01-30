8d4d33ebd053eeda7628f1bc527a26ab8b8dfaa0
Shubhithebeast <Shubhithebeast@gmail.com>
2024-01-30 13:59:26 +0530

    search by author done

--- QuoteApp/frontend/src/App.js
+++ QuoteApp/frontend/src/App.js
@@ -1,94 +1,106 @@
 // client/src/App.js
-import React, { useState, useEffect,useRef } from 'react';
-import axios from 'axios';
-import './App.css'; // Import the CSS file
-import Typed from 'typed.js'  
+import React, { useState, useEffect, useRef } from "react";
+import axios from "axios";
+import "./App.css"; // Import the CSS file
+import Typed from "typed.js";
 
-const  App=() =>{
-    const [quote, setQuote] = useState({ text: 'Loading...', author: 'Loading...' });
-    const [searchAuthor, setSearchAuthor] = useState('');
-    const autoText = useRef(null); 
+const App = () => {
+  const [quote, setQuote] = useState({
+    text: "Loading...",
+    author: "Loading...",
+  });
+  const [searchAuthor, setSearchAuthor] = useState("");
+  const autoText = useRef(null);
 
-    const getRandomQuote = async () => { 
-        try {
+  const getRandomQuote = async () => {
+    try {
+      const response = await axios.get("http://localhost:5000/api/random");
+      // console.log("Random quote fetch data: ",response.data);
+      // const {a:author, q:text} = response.data[0];
 
-            const response = await axios.get('http://localhost:5000/api/random');
-            // console.log("Random quote fetch data: ",response.data);
-            // const {a:author, q:text} = response.data[0];
+      // posting data , what we have displayed
+      // await axios.post('http://localhost:5000/api/addQuote',{author,text});
 
-            // posting data , what we have displayed
-            // await axios.post('http://localhost:5000/api/addQuote',{author,text});
-            
-            // setQuote({author,text});
-            setQuote(response.data[0]);
-        } catch (error) {  
-            console.error(error);
-        }
+      // setQuote({author,text});
+      setQuote(response.data[0]);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const searchByAuthor = async () => {
+    try {
+      const response = await axios.get(
+        `http://localhost:5000/api/search/${searchAuthor}`
+      );
+
+      console.log("Author quote fetch data: ", response.data);
+
+      if (response.data.text.length > 0) {
+        setQuote({
+          ...response.data,
+          text: response.data.text,
+          author: response.data.author,
+        });
+      } else {
+        setQuote({ text: "No quotes found.", author: "" });
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getRandomQuote();
+
+    const typed = new Typed(autoText.current, {
+      strings: [
+        "Quote of the day",
+        "Find your thoughts in quotes",
+        "Happiness, Love, Peace, Vibes, Calmness...",
+      ],
+      typeSpeed: 150,
+      backSpeed: 150,
+      loop: true,
+    });
+
+    return () => {
+      // Destroy Typed instance during cleanup to stop animation
+      typed.destroy();
     };
+  }, []);
 
-    const searchByAuthor = async () => {
-        try {
-            const response = await axios.get(`http://localhost:5000/api/search/${searchAuthor}`);
-            console.log("res->->",response.data);
-            // console.log("type->", typeof(response.data[0]));
+  return (
+    <div className="app-container">
+      <h1 class="font-effect-fire">
+        <span class="font-effect-neon">🖋 </span>
+        <span ref={autoText}></span>
+      </h1>
 
-            if (response.data.length !== 0) { 
-               console.log("Author quote fetch data: ",response.data);
-                setQuote(response.data[0]);
-            } else {
-               console.log("No quote found by Author: ",response.data);
-                setQuote({ text: 'No quotes found.', author: '' });
-            }
-        } catch (error) {
-            console.error(error);
-        }
-    };
+      <div className="quote-container">
+        <p className="quote-text">{quote.text}</p>
+        <p className="quote-author">-✍🏻{quote.author}</p>
+      </div>
 
-    useEffect(() => {
-        getRandomQuote();
+      <div className="buttons-container">
+        <button className="button" onClick={getRandomQuote}>
+          Next Quote
+        </button>
 
-        const typed = new Typed(autoText.current,{
-            strings:['Quote of the day','Find your thoughts in quotes','Happiness, Love, Peace, Vibes, Calmness...'],
-            typeSpeed:150,
-            backSpeed:150,
-            loop:true
-        })
-
-        return()=>{
-            // Destroy Typed instance during cleanup to stop animation
-            typed.destroy();
-        }
-
-    }, []);
-
-    return (
-        <div className="app-container">
-        <h1 class="font-effect-fire" ><span class="font-effect-neon">🖋 </span><span ref={autoText}></span></h1>
-            
-            <div className="quote-container">
-                <p className="quote-text">{quote.text}</p>
-                <p className="quote-author">-✍🏻{quote.author}</p>
-            </div>
-
-            <div className="buttons-container">
-                <button className="button" onClick={getRandomQuote}>
-                    Next Quote
-                </button>
-
-                <div className="search-container">
-                    <input
-                        type="text"
-                        placeholder="Enter author name"
-                        value={searchAuthor}
-                        onChange={(e) => setSearchAuthor(e.target.value)}
-                    />
-                    <button className="button" onClick={searchByAuthor}>
-                        Search by Author
-                    </button>
-                </div>
-            </div>
+        <div className="search-container">
+          <input
+            type="text"
+            placeholder="Enter author name"
+            value={searchAuthor}
+            onChange={(e) => setSearchAuthor(e.target.value)}
+          />
+          <button className="button" onClick={searchByAuthor}>
+            Search by Author
+          </button>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default App;
